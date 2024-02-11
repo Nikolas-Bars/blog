44b12118a5be6d87a35756d723c76f04413a512c
Nikolas-Bars~ <kasp2409@mail.ru>
2024-02-11 14:42:02 +0300

    fix delete blog route

--- src/validators/login-validator.ts
+++ src/validators/login-validator.ts
@@ -2,7 +2,7 @@ import {body} from "express-validator";
 import {inputValidatorMiddleware} from "../middlewares/input-validation-middleware";
 
 const authPasswordValidator = body('password')
-    .isString().withMessage('shortDescription must be string type').trim().isLength({min: 6, max: 20}).withMessage('incorrect login or password')
+    .isString().withMessage('incorrect login or password').trim().isLength({min: 6, max: 20}).withMessage('incorrect login or password')
 
 
 export const authValidator = () => {
