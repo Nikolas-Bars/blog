49b13fc506577fb2ad3e1129e0e4ee65476ec01e
Nikolas-Bars~ <kasp2409@mail.ru>
2024-02-18 13:16:29 +0300

    added logs

--- src/manager/email-manager.ts
+++ src/manager/email-manager.ts
@@ -6,7 +6,7 @@ export class EmailManager {
         const message = ` <h1>Thank for your registration</h1><a href=\'https://blog-t57v.onrender.com/confirm-email?code=${code}\'>complete registration</a>`
 
         const result: string | null = await emailAdapter.sendEmail(email, subject, message)
-
+        console.log(result, 'messageId')
         return result
     }
 }
