61c5e21b574a8cbcec4cae3df280d9001f21bc2c
Nikolas-Bars~ <kasp2409@mail.ru>
2024-03-06 16:14:57 +0300

    2fix

--- src/middlewares/refreshTokenMiddleware.ts
+++ src/middlewares/refreshTokenMiddleware.ts
@@ -14,7 +14,7 @@ export const refreshTokenMiddleware = async (req: Request, res: Response, next: 
     const payload: any = await JWTService.verifyRefreshToken(refreshToken)
 
     const black = await blackListRefreshCollection.findOne({token: refreshToken})
-
+    console.log(payload, black, 'payload, black')
     if (payload && !black) {
 
         const deviceId = payload.deviceId
@@ -23,10 +23,14 @@ export const refreshTokenMiddleware = async (req: Request, res: Response, next: 
 
         const deviceName = req.headers['user-agent'] ? req.headers['user-agent'] : 'unknown'
 
-        const session = await SessionServices.isSessionExists(deviceId, userId, payload.iat, deviceName!)
 
-        if (!session) return res.sendStatus(401)
+        // ТЕСТ
+        // const session = await SessionServices.isSessionExists(deviceId, userId, payload.iat, deviceName!)
+        //
+        // if (!session) return res.sendStatus(401)
 
+
+        /////////////
         const user = await usersCollection.findOne({_id: new ObjectId(userId)})
 
         if (!user) return res.sendStatus(401)
